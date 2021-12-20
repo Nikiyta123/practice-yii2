3d2cd974600eb4a5469689e5a8fe499bbc004637
Nikiyta123 <nik.rulit.01@mail.ru>
2021-12-20 16:07:34 +0800

    Change new

--- modules/user/views/backend/default/testwork.php
+++ modules/user/views/backend/default/testwork.php
@@ -1,24 +1,62 @@
 <?php
 
-$array = [
-    1 => 'Один',
-    3 => 'Два',
-    2 => 'Три',
-    4 => 'Четыре',
-    5 => 'Пять',
-];
-$start = microtime(true);
-    foreach ($array as $item){
-        debug($item);
+class myIterator implements Iterator {
+
+    private $array = [];
+
+    public function __construct($array) {
+        $this->array = $array;
     }
-echo 'Время выполнения скрипта: '.round(microtime(true) - $start, 5).' сек.';
+
+    public function rewind() {
+        var_dump(__METHOD__);
+        reset($this->array);
+    }
+
+    public function next() {
+        var_dump(__METHOD__);
+        $result = next($this->array);
+        return $result;
+    }
+
+    public function valid(): bool{
+        var_dump(__METHOD__);
+        $result = $this->current() !== false;
+        return $result;
+    }
+
+    public function current() {
+        var_dump(__METHOD__);
+        $result = current($this->array);
+        return $result;
+    }
+
+    public function key() {
+        var_dump(__METHOD__);
+        $result = key($this->array);
+        return $result;
+    }
+
+}
+
+$array = array(
+    0 => 'Швейцария',
+    1 => 'Германия',
+    2 => 'Индонезия',
+);
+
+$it = new myIterator($array);
+
+$start = microtime(true);
+
+    foreach($it as $key => $value) {
+        debug($value);
+    }
+
+echo xdebug_time_index() . ' sec.';
 
 
 
-//debug($array);
-//
-//$obj = new ArrayObject($array);
-//
-//$obj->ksort();
-//
-//debug($obj);
+
+
+
